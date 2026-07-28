2daab711d10154febc906736fe95d64ddec6aede
Duy Huynh <cduyhuynh@gmail.com>
2026-07-28 10:36:08 +0700

    Add: is-anagram - submission-6

---
+++ Data Structures & Algorithms/is-anagram/submission-6.go
@@ -0,0 +1,22 @@
+func isAnagram(s string, t string) bool {
+	var slen = len(s)
+	var tlen = len(t)
+	if (slen != tlen) {
+		return false
+	}
+	smap := make(map[rune]int, 26)
+	tmap := make(map[rune]int, 26)
+	for _, sval := range(s) {
+		smap[sval] += 1
+	}
+
+	for _, tval := range(t) {
+		tmap[tval] += 1
+	}
+	for skey, sval := range(smap) {
+		if sval != tmap[skey]{
+			return false
+		}
+	}
+	return true
+}
