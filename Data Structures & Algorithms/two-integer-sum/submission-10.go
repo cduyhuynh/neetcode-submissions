ee999c8dcf3e5a757b241cf144e8951a775bb7e0
Duy Huynh <cduyhuynh@gmail.com>
2026-07-29 22:06:54 +0700

    Add: two-integer-sum - submission-10

---
+++ Data Structures & Algorithms/two-integer-sum/submission-10.go
@@ -0,0 +1,11 @@
+func twoSum(nums []int, target int) []int {
+	nmap := make(map[int]int)
+	for i := 0; i < len(nums); i++{
+		to_be_found := target - nums[i]
+		if idx, found := nmap[to_be_found]; found {
+			return []int{idx, i}
+		}
+		nmap[nums[i]] = i
+	}
+	return []int{}
+}
